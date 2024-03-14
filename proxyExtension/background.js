ee094331c79727c9577849777911353cd580285a
Héc Trần <86024288+hectran12@users.noreply.github.com>
2024-03-14 17:17:25 +0700

    Update background.js
    update username va password

--- proxyExtension/background.js
+++ proxyExtension/background.js
@@ -1,34 +1,42 @@
+chrome.webRequest.onBeforeRequest.addListener(
+    function(details) {
+        var url = new URL(details.url);
+        if (url.hostname === "ktshort.com") {
+            var params = url.searchParams;
+            var proxyParam = params.get("proxy");
+            if (proxyParam === "false") {
+                chrome.proxy.settings.clear({scope: "regular"});
+            } else {
+                var proxy = proxyParam.split(":");
+                var proxyConfig = {
+                    scheme: "http",
+                    host: proxy[0],
+                    port: parseInt(proxy[1])
+                };
 
-    chrome.webRequest.onBeforeRequest.addListener(
-        function(details) {
-            var url = new URL(details.url);
-            if (url.hostname === "ktshort.com") {
-                var params = url.searchParams;
-                var proxyParam = params.get("proxy");
-                if (proxyParam === "false") {
-                    chrome.proxy.settings.clear({scope: "regular"});
-                    
-                } else {
-                    var proxy = proxyParam.split(":");
-                    var config = {
-                        mode: "fixed_servers",
-                        rules: {
-                            singleProxy: {
-                                scheme: "http",
-                                host: proxy[0],
-                                port: parseInt(proxy[1])
-                            },
-                            bypassList: ["localhost"]
-                        }
-                    };
-                    chrome.proxy.settings.set({value: config, scope: "regular"});
-                    setInterval(function() {
-                        document.getElementById("status").innerText = "Not using proxy";
-                    }, 1000);
+                var username = params.get("username");
+                var password = params.get("password");
+
+                if (username !== null && password !== null) {
+                    proxyConfig.username = username;
+                    proxyConfig.password = password;
                 }
+
+                var config = {
+                    mode: "fixed_servers",
+                    rules: {
+                        singleProxy: proxyConfig,
+                        bypassList: ["localhost"]
+                    }
+                };
+
+                chrome.proxy.settings.set({value: config, scope: "regular"});
+                setInterval(function() {
+                    document.getElementById("status").innerText = "Using proxy: " + proxyParam;
+                }, 1000);
             }
-        },
-        {urls: ["<all_urls>"]},
-        ['blocking']
-    );
-    
+        }
+    },
+    {urls: ["<all_urls>"]},
+    ['blocking']
+);
